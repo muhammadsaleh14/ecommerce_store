b3f32049c360ab61977866b6a73b0fb6c5fa2e16
Muhammad Saleh <salehmuhammadjahanzeb@gmail.com>
2026-05-30 00:57:29 +0500

    fixed provider

--- next-app/app/(store)/layout.tsx
+++ next-app/app/(store)/layout.tsx
@@ -1,7 +1,12 @@
+import { CartProvider } from '@/lib/context/cart'
 import { getTenant } from '@/tenants/registry'
 
 export default async function StoreLayout({ children }: { children: React.ReactNode }) {
   const tenant = await getTenant()
   const Layout = tenant.components.Layout
-  return <Layout>{children}</Layout>
+  return (
+    <CartProvider>
+      <Layout>{children}</Layout>
+    </CartProvider>
+  )
 }
